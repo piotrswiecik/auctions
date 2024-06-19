a06c9c37c93ab7e5414ad064ffa2f47f8790e9fa
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-19 19:22:09 +0200

    database dev config

--- auction_svc/src/auctions/config.py
+++ auction_svc/src/auctions/config.py
@@ -1,13 +1,21 @@
+import os
+
+from pydantic import Field, ValidationError
 from pydantic_settings import BaseSettings
 
 
+# TODO: handle validation/type errors
 class Settings(BaseSettings):
-    db_host: str = "localhost"
-    db_port: int = 5432
-    db_name: str = "auctions"
-    db_user: str = "development"
-    db_password: str = "development"
-    init_db: bool = True
+    db_host: str = os.environ.get("DB_HOST", "postgres")
+    db_port: int = int(os.environ.get("DB_PORT", 5432))
+    db_name: str = os.environ.get("DB_NAME", "auctions")
+    db_user: str = os.environ.get("DB_USER", "development")
+    db_password: str = os.environ.get("DB_PASSWORD", "development")
+    init_db: bool = bool(os.environ.get("INIT_DB", False))
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env.development"
 
 
 settings = Settings()
