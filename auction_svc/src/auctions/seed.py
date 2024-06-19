5e027b143102c4f4b0ff8ac5bd7d7cf6e508aa34
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-19 21:34:19 +0200

    add database seeder

--- auction_svc/src/auctions/seed.py
+++ auction_svc/src/auctions/seed.py
@@ -1,10 +1,11 @@
 """
 Database management tools - mostly for development.
 """
-
+import uuid
 import logging
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from auctions import models  # pylint: disable=unused-import
 from auctions.database import Base, get_db_conn_string
@@ -17,3 +18,30 @@ def init_db():
     with engine.begin() as conn:
         Base.metadata.drop_all(conn)
         Base.metadata.create_all(conn)
+
+    with Session(engine) as session:
+
+        item_1 = models.Item(
+            id=uuid.uuid4(),
+            make="Toyota",
+            model="Corolla",
+            year=2019,
+            color="blue",
+            mileage=10000,
+            image_url="https://via.placeholder.com/150",
+        )
+
+        item_2 = models.Item(
+            id=uuid.uuid4(),
+            make="Ford",
+            model="Fusion",
+            year=2018,
+            color="red",
+            mileage=20000,
+            image_url="https://via.placeholder.com/150",
+        )
+
+        session.add_all([item_1, item_2])
+        session.commit()
+
+        
